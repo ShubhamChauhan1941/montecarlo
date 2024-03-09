2861c061ff8a5dfab17102f73f53ac176e937afb
ShubhamChauhan1941 <golu.chouhan.68@gmail.com>
2024-03-09 22:17:11 +0530

    Test is done for montecarlo from 1000 to 1000000

--- testfile_montecarlo.py
+++ testfile_montecarlo.py
@@ -1,15 +1,15 @@
-def test_pi1(xvalues,n): 
-    from math import pi 
-    from pimotecarlo import montecarlopi 
-    xvalues=[]
-    x=10**6
-    er=0.02
+def test_1(): 
+	from math import pi 	
+	from pimotecarlo import montecarlopi 
+	xvalues=[1000,10000,100000,1000000]
+	for n in xvalues:
+		newmontecarlopi =montecarlopi(n)
+		pi_math=pi	
+		print(f"for n={n}, montecarlopi={newmontecarlopi} and math_pi={pi_math}") 
+		error = abs(newmontecarlopi - pi_math) / pi_math
+		er=error*100
+		print(er)
+		assert error < 2,f"Error for {n} points is larger than 20%: {er}"
+			
 
-    for i in range(10,x+1):
-        xvalues.append(i)
-        for n in xvalues: 
-            newmontecarlopi =montecarlopi(n)
-            pi_math=pi
-            error = abs(newmontecarlopi - pi_math) / pi_math
-            if error > 0.2:
-                print(f"Error for {n} points is larger than 20%: {error}")
+
